0a7cc4af78fda9c7097b21dd4e022f436d9641aa
Shaishav Gandhi <17shaishav@gmail.com>
2025-08-27 15:13:32 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ( ) {
+    int a,b,multiply;
+
+    printf("Enter value of a : ");
+    scanf("%d",&a);
+    printf("Enter value of b : ");
+    scanf("%d",&b);
+
+    multiply = a * b ;
+    printf("multiply is : %d",multiply);
+
+}
