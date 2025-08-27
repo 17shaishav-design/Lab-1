aa20254493bc73ced83716d6c7df399f9d59b24d
Shaishav Gandhi <17shaishav@gmail.com>
2025-08-27 21:29:02 +0530

    Create dollar into rupees.c

---
+++ dollar into rupees.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main ( ) {
+    float dollar, rupees;
+    printf("Enter value of dollar : ");
+    scanf("%f",&dollar);
+    rupees = dollar * 48 ;
+    printf("Rupees is : %f",rupees);
+}
