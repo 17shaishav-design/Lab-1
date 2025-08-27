8a0abb861947418c7d73f11c50bbb70490b405e3
Shaishav Gandhi <17shaishav@gmail.com>
2025-08-27 15:20:28 +0530

    Create divide.c

---
+++ divide.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ( ) {
+    int a,b,divide;
+
+    printf("Enter value of a : ");
+    scanf("%d",&a);
+    printf("Enter value of b : ");
+    scanf("%d",&b);
+
+    divide = a / b ;
+    printf("Divide is : %d",divide);
+
+}
